50a0f18dbd528cee3059e496c2756754ba7dc7fe
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-15 22:32:00 -0300

    Criando a classe LeitorDeArquivo

---
+++ ByteBank/LeitorDeArquivo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivo
+    {
+        public string Arquivo { get; }
+        
+        public LeitorDeArquivo(string Arquivo)
+        {
+            this.Arquivo = Arquivo;
+            Console.WriteLine($"Abrindo arquivo: {Arquivo}");
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha");
+            return "Linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
